d3cc6265078cf3242e06893347df8212a6215405
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-10-06 12:20:09 +0300

    4

--- 4.py
+++ 4.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv("data.csv", delimiter=",")
+df["расстояние_кат"] = df["расстояние"].map(
+    lambda x: "домашний_регион" if int(x) <= 300 else "недалеко_отдома" if int(x) > 300 and int(
+        x) <= 700 else "дальнее_путешествие")
+print(round(df["расстояние_кат"].value_counts(normalize=True) * 100).astype(int).to_frame())
