4b1286190bc8c2b35a60fb41b496483cb1be8c8c
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-10-19 00:44:29 +0300

    crosstab

---
+++ 3/5.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import scipy
+
+df = pd.read_csv("qq_csv_lms.csv", delimiter=",", skipinitialspace=True, encoding="cp1251")
+df.columns = [i.lower().replace(" ", "_") for i in df.columns]
+df = df[(df["ингредиент_1"] < 270) & (df["номер_смены"] == 2)]
+xi = scipy.stats.chi2_contingency(pd.crosstab(df["оператор_линии"], df["брак"]))
+kramer = (xi[0] / (len(df) * (min(pd.crosstab(df["оператор_линии"], df["брак"]).shape)
+                              - 1))) ** 0.5
+print(round(kramer, 4))
+
